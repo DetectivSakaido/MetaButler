381b392036e6a509c557eeda64d8a7877a1245a5
destroyer19991 <bpsgusain@gmail.com>
2021-02-03 19:00:00 +0530

    removed /fbanlist command from help

--- metabutler/modules/feds.py
+++ metabutler/modules/feds.py
@@ -854,157 +854,6 @@ def get_chat(chat_id, chat_data):
     except KeyError:
         return {"status": False, "value": False}
 
-@run_async
-def fed_ban_list(bot: Bot, update: Update, args: List[str], chat_data):
-    chat = update.effective_chat
-    user = update.effective_user
-
-    fed_id = sql.get_fed_id(chat.id)
-    info = sql.get_fed_info(fed_id)
-
-    if not fed_id:
-        update.effective_message.reply_text(
-            "This group is not a part of any federation!")
-        return
-
-    if is_user_fed_owner(fed_id, user.id) == False:
-        update.effective_message.reply_text(
-            "Only Federation owners can do this!")
-        return
-
-    user = update.effective_user
-    chat = update.effective_chat
-    getfban = sql.get_all_fban_users(fed_id)
-    if len(getfban) == 0:
-        update.effective_message.reply_text(
-            "The federation ban list of {} is empty".format(info['fname']),
-            parse_mode=ParseMode.HTML)
-        return
-
-    if args:
-        if args[0] == 'json':
-            jam = time.time()
-            new_jam = jam + 1800
-            cek = get_chat(chat.id, chat_data)
-            if cek.get('status'):
-                if jam <= int(cek.get('value')):
-                    waktu = time.strftime("%H:%M:%S %d/%m/%Y",
-                                          time.localtime(cek.get('value')))
-                    update.effective_message.reply_text(
-                        "You can backup your data once every 30 minutes!\nYou can back up data again at `{}`"
-                        .format(waktu),
-                        parse_mode=ParseMode.MARKDOWN)
-                    return
-                else:
-                    if user.id not in SUDO_USERS:
-                        put_chat(chat.id, new_jam, chat_data)
-            else:
-                if user.id not in SUDO_USERS:
-                    put_chat(chat.id, new_jam, chat_data)
-            backups = ""
-            for users in getfban:
-                getuserinfo = sql.get_all_fban_users_target(fed_id, users)
-                json_parser = {
-                    "user_id": users,
-                    "first_name": getuserinfo['first_name'],
-                    "last_name": getuserinfo['last_name'],
-                    "user_name": getuserinfo['user_name'],
-                    "reason": getuserinfo['reason']
-                }
-                backups += json.dumps(json_parser)
-                backups += "\n"
-            with BytesIO(str.encode(backups)) as output:
-                output.name = "saitama_fbanned_users.json"
-                update.effective_message.reply_document(
-                    document=output,
-                    filename="saitama_fbanned_users.json",
-                    caption="Total {} User are blocked by the Federation {}.".
-                    format(len(getfban), info['fname']))
-            return
-        elif args[0] == 'csv':
-            jam = time.time()
-            new_jam = jam + 1800
-            cek = get_chat(chat.id, chat_data)
-            if cek.get('status'):
-                if jam <= int(cek.get('value')):
-                    waktu = time.strftime("%H:%M:%S %d/%m/%Y",
-                                          time.localtime(cek.get('value')))
-                    update.effective_message.reply_text(
-                        "You can back up data once every 30 minutes!\nYou can back up data again at `{}`"
-                        .format(waktu),
-                        parse_mode=ParseMode.MARKDOWN)
-                    return
-                else:
-                    if user.id not in SUDO_USERS:
-                        put_chat(chat.id, new_jam, chat_data)
-            else:
-                if user.id not in SUDO_USERS:
-                    put_chat(chat.id, new_jam, chat_data)
-            backups = "id,firstname,lastname,username,reason\n"
-            for users in getfban:
-                getuserinfo = sql.get_all_fban_users_target(fed_id, users)
-                backups += "{user_id},{first_name},{last_name},{user_name},{reason}".format(
-                    user_id=users,
-                    first_name=getuserinfo['first_name'],
-                    last_name=getuserinfo['last_name'],
-                    user_name=getuserinfo['user_name'],
-                    reason=getuserinfo['reason'])
-                backups += "\n"
-            with BytesIO(str.encode(backups)) as output:
-                output.name = "saitama_fbanned_users.csv"
-                update.effective_message.reply_document(
-                    document=output,
-                    filename="saitama_fbanned_users.csv",
-                    caption="There are {} users blocked in {} federation.".
-                    format(len(getfban), info['fname']))
-            return
-
-    text = "<b>{} users have been banned from the federation {}:</b>\n".format(
-        len(getfban), info['fname'])
-    for users in getfban:
-        getuserinfo = sql.get_all_fban_users_target(fed_id, users)
-        if getuserinfo == False:
-            text = "There are no users banned from the federation {}".format(
-                info['fname'])
-            break
-        user_name = getuserinfo['first_name']
-        if getuserinfo['last_name']:
-            user_name += " " + getuserinfo['last_name']
-        text += " • {} (<code>{}</code>)\n".format(
-            mention_html(users, user_name), users)
-
-    try:
-        update.effective_message.reply_text(text, parse_mode=ParseMode.HTML)
-    except Exception:
-        jam = time.time()
-        new_jam = jam + 1800
-        cek = get_chat(chat.id, chat_data)
-        if cek.get('status'):
-            if jam <= int(cek.get('value')):
-                waktu = time.strftime("%H:%M:%S %d/%m/%Y",
-                                      time.localtime(cek.get('value')))
-                update.effective_message.reply_text(
-                    "You can back up data once every 30 minutes!\nYou can back up data again at `{}`"
-                    .format(waktu),
-                    parse_mode=ParseMode.MARKDOWN)
-                return
-            else:
-                if user.id not in SUDO_USERS:
-                    put_chat(chat.id, new_jam, chat_data)
-        else:
-            if user.id not in SUDO_USERS:
-                put_chat(chat.id, new_jam, chat_data)
-        cleanr = re.compile('<.*?>')
-        cleantext = re.sub(cleanr, '', text)
-        with BytesIO(str.encode(cleantext)) as output:
-            output.name = "fbanlist.txt"
-            update.effective_message.reply_document(
-                document=output,
-                filename="fbanlist.txt",
-                caption=
-                "The following is a list of users who are currently fbanned in the Federation {}."
-                .format(info['fname']))
-
 
 __help__ = True
 
@@ -1025,7 +874,6 @@ FED_CHAT_HANDLER = CommandHandler("chatfed", fed_chat, pass_args=True)
 FED_ADMIN_HANDLER = CommandHandler("fedadmins", fed_admin, pass_args=True)
 FED_NOTIF_HANDLER = CommandHandler("fednotif", fed_notif, pass_args=True)
 FED_CHATLIST_HANDLER = CommandHandler("fedchats", fed_chats, pass_args=True)
-FED_USERBAN_HANDLER = CommandHandler("fbanlist", fed_ban_list, pass_args=True, pass_chat_data=True)
 
 DELETEBTN_FED_HANDLER = CallbackQueryHandler(del_fed_button, pattern=r"rmfed_")
 
@@ -1044,6 +892,5 @@ dispatcher.add_handler(FED_CHAT_HANDLER)
 dispatcher.add_handler(FED_ADMIN_HANDLER)
 # dispatcher.add_handler(FED_NOTIF_HANDLER)
 # dispatcher.add_handler(FED_CHATLIST_HANDLER)
-dispatcher.add_handler(FED_USERBAN_HANDLER)
 
 dispatcher.add_handler(DELETEBTN_FED_HANDLER)
